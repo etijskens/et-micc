eb1ce3a0f1b4b52ad2b419eca112558e83f85fd1
Engelbert Tijskens <engelbert.tijskens@uantwerpen.be>
2019-12-02 13:29:33 +0100

    v0.9.36

--- et_micc/__init__.py
+++ et_micc/__init__.py
@@ -6,6 +6,6 @@ Package et-micc
 
 Top-level package for :py:mod:`et-micc`.
 """
-__version__ = "0.9.35"
+__version__ = "0.9.36"
 
 import et_micc.cli_micc # avoid attribute error when executing micc when installed from pypi
