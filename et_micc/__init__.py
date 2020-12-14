dfbe54b39dabbc719d6f8682400243e296bd9254
Engelbert Tijskens <engelbert.tijskens@uantwerpen.be>
2020-12-14 23:26:41 +0100

    version bump
    put cpp and f2py binary extensions on the same footing

--- et_micc/__init__.py
+++ et_micc/__init__.py
@@ -6,6 +6,6 @@ Package et-micc
 
 Top-level package for :py:mod:`et-micc`.
 """
-__version__ = "0.10.31"
+__version__ = "1.0.0"
 
 import et_micc.cli_micc # avoid attribute error when executing micc when installed from pypi
